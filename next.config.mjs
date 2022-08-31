dd74bdbcb02a542e1b8f62d219207f4bb02dfbea
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-08-31 13:48:53 +0300

    chore(config): move to next.config.mjs to esm

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
     reactStrictMode: true,
     swcMinify:       true,
-    eslint:          {
+
+    eslint: {
         ignoreDuringBuilds: true,
     },
 };
-
-module.exports = nextConfig;
