8d8265870a33c8004adc7ac78f3e278bf43c248c
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-01 23:12:12 +0300

    chore(deps): connect @testing-library/react

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    extensionsToTreatAsEsm: [ '.ts' ],
+    extensionsToTreatAsEsm: [ '.ts', '.tsx' ],
     transform:              {
         '^.+\\.(t|j)sx?$': '@swc/jest',
     },
