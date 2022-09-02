9fe5bae0d257804c86de9a49840ab35faa48d94e
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-03 01:32:45 +0300

    feat(ci): connect github action to run cypress in CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-    projectId: 'h7cu6j',
+    projectId: '4i9jo4',
     e2e:       {
         baseUrl: 'http://localhost:3000',
         setupNodeEvents(on, config) {
