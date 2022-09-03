1a6d02b3a76e6fbad764238bf2f01ea12f7a00bf
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-03 15:24:09 +0300

    chore(deps): install local-cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
-/// <reference types="cypress" />
+/* Core */
+import { Cypress, cy } from 'local-cypress';
 
 // ***********************************************
 // This example commands.ts shows you how to
