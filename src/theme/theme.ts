c11b3c703927c266548fa03b51a0fa2e43a6bf3b
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-05 23:18:38 +0300

    refactor(style): extract color palette to separate file

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,15 +1,8 @@
 /* Core */
 import type { Theme } from 'styled-components';
 
-const palette = {
-    'color-1': '#000000',
-    'color-2': '#101010',
-    'color-3': '#FFFFFF',
-    'color-4': '#FAFAFA',
-    'color-5': '#F1F1F1',
-    'color-6': '#D87D4A',
-    'color-7': '#FBAF85',
-};
+/* Instruments */
+import { palette } from './palette';
 
 export const theme: Theme = {
     palette,
