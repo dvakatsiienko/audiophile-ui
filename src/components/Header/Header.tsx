abc9521ec44e08c7fc5735021e6a2dc721c92913
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-08 23:07:29 +0300

    feat(core): implement <Header /> component with desktop viewport size support

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,51 @@
 /* Core */
 import Link from 'next/link';
-import Image from 'next/future/image';
-import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 /* Instruments */
-import logo from './img/audiophile-logo.svg';
+import { LogoSvg, ShoppingCartSvg } from './img';
 
 export const Header = () => {
-    const router = useRouter();
-
     return (
         <SHeader>
-            <Image alt = 'logo' src = { logo } />
+            <section>
+                <Link href = '/'>
+                    <a>
+                        <LogoSvg />
+                    </a>
+                </Link>
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link href = '/'>
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href = '/headphones'>
-                            <a>Headphones</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href = '/speakers'>
-                            <a>Speakres</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href = '/earphones'>
-                            <a>Earphones</a>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href = '/'>
+                                <a>Home</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href = '/headphones'>
+                                <a>Headphones</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href = '/speakers'>
+                                <a>Speakres</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href = '/earphones'>
+                                <a>Earphones</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
 
-            <Cart onClick = { () => router.push('/checkout') } />
+                <Link href = '/checkout'>
+                    <a>
+                        <ShoppingCartSvg />
+                    </a>
+                </Link>
+            </section>
         </SHeader>
     );
 };
@@ -47,40 +53,50 @@ export const Header = () => {
 /* Styles */
 const SHeader = styled.header`
     grid-area: header;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    padding: 0 50px;
-    background-color: ${p => p.theme.palette[ 'color-2' ]};
+    background-color: var(--color-2);
     color: white;
+    height: 96px;
 
-    & h1 {
-        justify-self: start;
-    }
+    & section {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin: auto;
+        height: 100%;
+        border-bottom: 1px solid #333333;
+        width: ${p => p.theme.viewports.desktop};
 
-    & nav {
-        & ul {
-            display: flex;
-            gap: 34px;
+        & svg {
+            cursor: pointer;
 
-            & li {
-                & a {
-                    font-size: 13px;
-                    font-weight: 700;
-                    line-height: 25px;
-                    letter-spacing: 2px;
+            &:hover {
+                & path {
+                    transition: fill 300ms ease;
+                    fill: var(--color-6);
+                }
+            }
+        }
+
+        & nav {
+            & ul {
+                display: flex;
+                gap: 34px;
+
+                & li {
+                    & a {
+                        font-size: 13px;
+                        font-weight: 700;
+                        line-height: 25px;
+                        letter-spacing: 2px;
+                        transition: color 300ms ease;
+
+                        &:hover {
+                            color: var(--color-6);
+                        }
+                    }
                 }
             }
         }
     }
-`;
-
-const Cart = styled.div`
-    width: 16px;
-    height: 16px;
-    background-color: #fff;
-    border-radius: 50%;
-    cursor: pointer;
 `;
--- types/styled.d.ts
+++ types/styled.d.ts
@@ -1,5 +1,6 @@
 /* Core */
 import 'styled-components';
+import type {} from 'styled-components/cssprop';
 
 declare module 'styled-components' {
     export interface DefaultTheme {
