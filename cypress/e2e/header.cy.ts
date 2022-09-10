c088db7ab7a21c5d2d61999aac72eb89e8ce6689
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2022-09-11 00:40:24 +0300

    feat(test): write e2e test suite for <Header />

---
+++ cypress/e2e/header.cy.ts
@@ -0,0 +1,49 @@
+/* Core */
+import { cy, describe, it, beforeEach, expect } from 'local-cypress';
+
+/* Instruments */
+// import { mobile } from '@/ui';
+import { mobile } from '../../src/ui';
+
+describe('<Header />:', () => {
+    it('opens a mobile device by clicking on Burger Menu Icon', () => {
+        cy.visit('/');
+
+        const nav = cy.get('.nav');
+        const logoSvg = cy.get('[data-testid=logo-svg]');
+        const shoppingCartSvg = cy.get('[data-testid=shopping-cart-svg');
+
+        cy.viewport(1440, 750);
+
+        nav.should('exist');
+        logoSvg.should('exist');
+        shoppingCartSvg.should('exist');
+
+        cy.viewport(375, 750);
+        const burger = cy.get('[data-testid=burger-menu-svg]');
+
+        burger.should('exist');
+        burger.click();
+
+        const mobileViewModal = cy.get('[data-testid=mobile-nav-modal');
+        mobileViewModal.should('exist');
+    });
+
+    it('navigates to a target route once link is clicked', async () => {
+        const lis = cy.get('li');
+        lis.eq(1).click();
+        cy.url().should('eq', 'http://localhost:3000/speakers');
+    });
+
+    it('closes mobile modal once Escape key is pressed', async () => {
+        cy.viewport(375, 750);
+        const burger = cy.get('[data-testid=burger-menu-svg]');
+        burger.click();
+        const mobileViewModal = cy.get('[data-testid=mobile-nav-modal]');
+        mobileViewModal.should('exist');
+        cy.type('{esc}');
+        cy.get('.header-section').should('not.have.descendants', '[data-testid=mobile-nav-modal]');
+    });
+});
+
+export {};
